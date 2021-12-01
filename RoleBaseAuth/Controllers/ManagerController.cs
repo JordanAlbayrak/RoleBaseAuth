6842ceae54ebde4c6156f97e0d49489448aa307a
Jordan Alba <jordan008@live.ca>
2021-12-01 14:16:53 -0500

    Add project files.

---
+++ RoleBaseAuth/Controllers/ManagerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoleBaseAuth.Controllers
+{
+    public class ManagerController : Controller
+    {
+        [Authorize(Roles="Manager")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
